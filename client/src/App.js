00d4ed3bbbd5578c8cf0130d84c9fcab3e1fce92
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-09-06 14:23:12 -0400

    read questions and display on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { getData, getQuestion } from './data/quiz';
+import { useState } from 'react';
 function App() {
+  const [questionId, setQuestionId] = useState(2);
+  const currentQuestion = getQuestion(questionId);
   return (
     <>
       <header className="App">
-        app Header
+        {/* app Header */}
       </header>
-      {/* question */}
-      {/* answers */}
-      <Outlet />
+      <main>
+        <article>
+          <h3>Question {questionId}/8</h3>
+          <p>{currentQuestion.question}</p>
+        </article>
+        <section>
+          {
+            currentQuestion.answers.map((answer) =>
+              <div className='answer'>
+                {/* use counterReset to set list order A, B, C, D */}
+                <p>{answer}</p>
+              </div>
+            )
+          }
+        </section>
+        <button className='next-btn'>Next</button>
+        <Outlet />
+      </main>
       <footer>
-        app footer
+        {/* app footer */}
       </footer>
     </>
   );
