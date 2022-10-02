40329d1fa8071447d21f665d0adf2921da2dff59
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-10-02 11:57:29 -0400

    create user APi

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import questionRouter from './questions-router'
+import userRouter from './users-router'
 
 const router: Router = Router();
 
 router.use('/questions', questionRouter);
+router.use('/users', userRouter);
 
 export default router;
