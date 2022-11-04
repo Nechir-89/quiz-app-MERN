23ad9ecc92af18a8772a6ce5ed51bad499c530ea
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-11-04 17:22:30 -0400

    add transparent background to popup container

--- client/src/components/Popup/PopUp.jsx
+++ client/src/components/Popup/PopUp.jsx
@@ -4,31 +4,35 @@ import templateText from './popup-texts';
 
 export default function PopUp(props) {
   return props.time === "start" ? (
-    <article className='popup'>
-      <h3>{templateText.start.title}</h3>
-      <section>
-        {
-          React.Children.toArray(
-            templateText.start.par.map((p) => (<p>{p}</p>))
-          )
-        }
-      </section>
-      <button>{templateText.start.btnText}</button>
-    </article>
-  )
-    :
-    (
-      <article>
-        <h3>{templateText.end.title}</h3>
+    <div className='popup-container'>
+      <article className='popup'>
+        <h3>{templateText.start.title}</h3>
         <section>
           {
             React.Children.toArray(
-              templateText.end.par.map((p) => (<p>{p}</p>))
+              templateText.start.par.map((p) => (<p>{p}</p>))
             )
           }
-          <p>{props.result}</p>
         </section>
-        <button>{templateText.end.btnText}</button>
+        <button>{templateText.start.btnText}</button>
       </article>
+    </div>
+  )
+    :
+    (
+      <div className='popup-container'>
+        <article>
+          <h3>{templateText.end.title}</h3>
+          <section>
+            {
+              React.Children.toArray(
+                templateText.end.par.map((p) => (<p>{p}</p>))
+              )
+            }
+            <p>{props.result}</p>
+          </section>
+          <button>{templateText.end.btnText}</button>
+        </article>
+      </div>
     )
 }
