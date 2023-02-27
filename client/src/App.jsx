7c561061018479b2da477fbd4cb6397e6e4de4da
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2023-02-27 12:19:43 -0500

    commenting out google sign in code form app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
+// import jwt_decode from 'jwt-decode';
 import './App.css';
 
 // global state
@@ -9,57 +9,57 @@ import './App.css';
 function App() {
   // console.log(process.env.NODE_ENV);
 
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  function handleCredentialResponse(response) {
-    const usr = jwt_decode(response.credential);
-    console.log(usr)
-    setUser(() => usr)
-  }
+  // function handleCredentialResponse(response) {
+  //   const usr = jwt_decode(response.credential);
+  //   console.log(usr)
+  //   setUser(() => usr)
+  // }
 
-  useEffect(() => {
-    window.google.accounts.id.initialize({
-      client_id: '33005118597-p6k0cstkacrfjf2etue4euodlgfs5k63.apps.googleusercontent.com',
-      // auto_select: false,
-      // login_uri: 'http://localhost:3000/login',
-      prompt_parent_id: 'google_signin_btn_container',
-      // context: 'signup',
-      // ux_mode: 'redirect',
-      callback: handleCredentialResponse
-    });
+  // useEffect(() => {
+  //   window.google.accounts.id.initialize({
+  //     client_id: '33005118597-p6k0cstkacrfjf2etue4euodlgfs5k63.apps.googleusercontent.com',
+  //     // auto_select: false,
+  //     // login_uri: 'http://localhost:3000/login',
+  //     prompt_parent_id: 'google_signin_btn_container',
+  //     // context: 'signup',
+  //     // ux_mode: 'redirect',
+  //     callback: handleCredentialResponse
+  //   });
 
-    const signinEle = document.getElementById('google_signin_btn_container');
-    window.google.accounts.id.renderButton(signinEle, {
-      theme: 'outline', // filled_blue, filled_black
-      type: 'standard', // icon
-      text: 'signup_with', // signin_with, signup_with, continue_with, use
-      size: 'medium', // default: large, small, medium
-      shape: 'pill', // default: rectangular, circle, square
-      logo_alignment: 'center', // default: left, center
-      width: '100', // width in px, the max value is 400px
+    // const signinEle = document.getElementById('google_signin_btn_container');
+    // window.google.accounts.id.renderButton(signinEle, {
+    //   theme: 'outline', // filled_blue, filled_black
+    //   type: 'standard', // icon
+    //   text: 'signup_with', // signin_with, signup_with, continue_with, use
+    //   size: 'medium', // default: large, small, medium
+    //   shape: 'pill', // default: rectangular, circle, square
+    //   logo_alignment: 'center', // default: left, center
+    //   width: '100', // width in px, the max value is 400px
       // locale: 'en-usa',
       // click_listner: handleClickEvent
-    });
+    // });
     // window.google.accounts.id.prompt();
 
-  }, []);
+  // }, []);
 
   return (
     <>
       <header className="App">
         {/* <a href="/login">Login</a> */}
         {
-          !user ?
-            <div id='google_signin_btn_container'></div>
-            :
-            <>
-              <img src={user.picture} alt={user.name} />
-              <h3>{user.name}</h3>
-              <button onClick={() => {
-                window.google.accounts.id.disableAutoSelect();
-                setUser(() => null);
-              }}>Sign Out</button>
-            </>
+          // !user ?
+          //   <div id='google_signin_btn_container'></div>
+          //   :
+          //   <>
+          //     <img src={user.picture} alt={user.name} />
+          //     <h3>{user.name}</h3>
+          //     <button onClick={() => {
+          //       window.google.accounts.id.disableAutoSelect();
+          //       setUser(() => null);
+          //     }}>Sign Out</button>
+          //   </>
         }
       </header>
       <Outlet />
